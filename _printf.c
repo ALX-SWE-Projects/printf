48f2001e64f8705991cb08ff26c7444403d095ec
ayyyub777 <140520247+ayyyub777@users.noreply.github.com>
2023-11-13 22:16:55 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -6,12 +6,40 @@
  *
  * Return: The number of characters printed
  */
-int _printf(const char *format, ...)
-{
-va_list args;
+int _printf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
 
-if (format == NULL)
-return (-1);
+    int count = 0;
 
+    while (*format) {
+        if (*format == '%') {
+            format++;
+            switch (*format) {
+                case 'c':
+                    count += putchar(va_arg(args, int));
+                    break;
+                case 's': {
+                    const char *str = va_arg(args, const char *);
+                    while (*str) {
+                        count += putchar(*str);
+                        str++;
+                    }
+                    break;
+                }
+                case '%':
+                    count += putchar('%');
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            count += putchar(*format);
+        }
 
+        format++;
+    }
+
+    va_end(args);
+    return count;
 }
